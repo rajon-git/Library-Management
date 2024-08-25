edd6b0c7730eaa7ba33d80ad55e8770cb2bc4d96
rajon-git <rajon.zhsust15@gmail.com>
2024-08-25 23:15:41 +0600

    login and register added

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_join', 'is_active')
+    list_display_links = ('email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_join')
+    ordering = ('-date_join',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(models.Account, AccountAdmin)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from .models import Account
+from django.contrib import messages, auth
+import requests
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password
+            )
+            user.phone_number = phone_number
+            user.save()
+
+            messages.success(
+                request,
+                'Thank you for registering with us. We have sent you a verification email to your email address. Please verify it.'
+            )
+            return redirect('login')  # Replace 'success_url' with the name of the URL you want to redirect to
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'author/register.html', context)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        email= request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are logged in.')
+        else:
+            messages.error(request, 'Invalid login creadentials')
+            return redirect('login')
+    return render(request, 'author/login.html')
